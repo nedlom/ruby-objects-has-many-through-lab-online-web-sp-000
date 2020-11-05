fa53be76b8780a86f1c2767b68b58422dbd2c850
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 16:46:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,15 @@ class Artist
 
   attr_accessor :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
   end
   
 end
