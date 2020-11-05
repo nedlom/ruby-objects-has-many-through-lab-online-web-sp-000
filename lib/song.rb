e630b628deebfa09696b56b9ebbbf49007ac7c14
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 16:44:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,17 @@
-class Song
+# class Song
   
-  attr_accessor :name, :artist, :genre
+#   attr_accessor :name, :artist, :genre
   
-  @@all = []
+#   @@all = []
   
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@all << self
-  end
+#   def initialize(name, artist, genre)
+#     @name = name
+#     @artist = artist
+#     @genre = genre
+#     @@all << self
+#   end
   
-  def self.all
-    @@all
-  end
-end
+#   def self.all
+#     @@all
+#   end
+# end
