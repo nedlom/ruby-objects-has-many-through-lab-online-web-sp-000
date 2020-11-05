dd3f46397d4d8474141c85dd435c1f9fe5aee075
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-05 16:45:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,36 @@
+class Artist
+
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # class Artist
   
 #   attr_accessor :name
